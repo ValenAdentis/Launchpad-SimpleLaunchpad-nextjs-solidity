fcff0ee6a9dc991bc87d50936c2f973d26c130e1
Your Name <your_verified_github_email@example.com>
2026-09-22 01:47:13 -0400

    third commit

--- frontend/src/lib/wagmi.ts
+++ frontend/src/lib/wagmi.ts
@@ -1,6 +1,6 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { http } from "wagmi";
-import { localhost, mainnet, sepolia } from "wagmi/chains";
+import { base, baseSepolia, localhost } from "wagmi/chains";
 
 const projectId =
   process.env.NEXT_PUBLIC_WC_PROJECT_ID || "00000000000000000000000000000000";
@@ -8,11 +8,11 @@ const projectId =
 export const wagmiConfig = getDefaultConfig({
   appName: "LaunchPad Simple",
   projectId,
-  chains: [localhost, sepolia, mainnet],
+  chains: [base, baseSepolia, localhost],
   transports: {
+    [base.id]: http(),
+    [baseSepolia.id]: http(),
     [localhost.id]: http("http://127.0.0.1:8545"),
-    [sepolia.id]: http(),
-    [mainnet.id]: http(),
   },
   ssr: true,
 });
